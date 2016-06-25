f55338102a8af9ab20c516ade93429e9d54f2c01
ro-double-b <robb.marsee@gmail.com>
2016-06-24 19:59:38 -0700

    -Render App component to the DOM. -Pass exampleVideoData to the VideoList component. -Refactor VideoList to render each VideoListEntry component. -Pass song video into VideoPlayer. -Refactor App component using ES6.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      collection: window.exampleVideoData,
+      currentSong: window.exampleVideoData[0]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentSong}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList currentVideo={this.state.currentSong} videos={this.state.collection}/>
+        </div>
+      </div>
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
